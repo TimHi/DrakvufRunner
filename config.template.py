d4589a0c1b2063fe7adea1d9a5d75a51bca5dbb0
TimHi <TimHiller@live.de>
2019-04-24 20:42:35 +0200

    config template

---
+++ config.template.py
@@ -0,0 +1,8 @@
+#Full path to the created vm config
+WINDOWS_CONFIG_LOCATION = ""
+#Full path to the drakvuf installation i.e /home/user/drakvuf/src/
+DRAKVUF_LOCATION = ""
+#IP for gvncviewer
+VM_IP = ""
+#Full path to the desired rekall profile
+REKALL_PROFILE_LOCATION = ""
