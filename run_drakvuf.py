d49d7c69a736b7ae6d98cfd7af42aadc9409035a
TimHi <TimHiller@live.de>
2019-04-24 21:10:15 +0200

    Drakvuf runs

--- run_drakvuf.py
+++ run_drakvuf.py
@@ -5,13 +5,23 @@
 ############################################################
 
 import sys
+import subprocess
 from xen_utils import get_xen_domain
 from config import DRAKVUF_LOCATION
 from config import REKALL_PROFILE_LOCATION
 
+def run_command(drakvuf_command):
+    subprocess.run([drakvuf_command], shell=True)
+
+
 def build_drakvuf_command(program_arguments):
-    drakvuf_command = DRAKVUF_LOCATION + " -r " + REKALL_PROFILE_LOCATION
+    drakvuf_command = "sudo " + DRAKVUF_LOCATION + "drakvuf -r " + REKALL_PROFILE_LOCATION
     domain = get_xen_domain()
+    drakvuf_command += " -d " + domain
+    #TODO: Parse arguments
+
+    #We're done building Frankensteins Monster, run it
+    run_command(drakvuf_command) 
 def main():
     for arg in sys.argv[1:]:
         print(arg)
