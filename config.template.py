6e2e4ee8b590405781c574eb9728139ffcb70fa0
TimHi <TimHiller@live.de>
2019-04-26 12:28:45 +0200

    config template

--- config.template.py
+++ config.template.py
@@ -6,3 +6,17 @@ DRAKVUF_LOCATION = ""
 VM_IP = ""
 #Full path to the desired rekall profile
 REKALL_PROFILE_LOCATION = ""
+#Folder for extracted files (-D flag)
+DUMP_FOLDER = ""
+#Injection method for -m flag (createproc, shellexec, shellcode, doppelganging)
+INJECTION_METHOD = ""
+#List of Plugins to activate available are: objmon syscall poolmon socketmon exmon filetracer filedelete ssdtmon
+ACTIVE_PLUGINS = ['objmon']
+#List of Plugins that are deactivated
+DEACTIVATED_PLUGINS = ['syscall', 'poolmon', 'socketmon', 'exmon', 'filetracer', 'filedelete', 'ssdtmon']
+#The executable to start with injection
+INJECT_FILE = ""
+#Timeout of execution for -t flag in seconds
+TIMEOUT = ""
+#Output method (default, csv, kv, or json)
+OUTPUT_FORMAT = ""
